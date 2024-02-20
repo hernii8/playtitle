e13d5482da7a311806d3aa7b47e6eb1a76ff8d5e
hernii8 <hernandelafuentelopez@gmail.com>
2024-02-20 12:55:06 +0100

    Prompt changes

--- playtitle/use_cases/generate_title_for_spotify_playlist.py
+++ playtitle/use_cases/generate_title_for_spotify_playlist.py
@@ -15,6 +15,6 @@ class GenerateTitleForSpotifyPlaylist:
     def __build_prompt(self):
         average_values = self.playlist.get_average_values()
         return f"""Craft a title for a playlist that embodies the genres of {", ".join(average_values["top3_genres"])}, featuring the iconic sounds of {", ".join(average_values["top3_artists"])}. 
-            Picture {average_values["total"]} tracks, each averaging {average_values["duration"]} in duration, with {average_values["loudness"]} decibels and {average_values["tempo"]}bpm tempo. 
+            Picture {average_values["total"]} tracks, each averaging {average_values["duration"]}ms in duration and {average_values["tempo"]}bpm tempo. 
             From 0 to 1, the playlist is {average_values["acousticness"]} acoustic, {average_values["danceability"]} danceable, {average_values["energy"]} energetic, {average_values["instrumentalness"]} instrumental and {average_values["happiness"]} happy.
             What title captures the essence of this playlist's vibe? It's important that you respond with only the title, nothing else. Maximum 5 words."""
